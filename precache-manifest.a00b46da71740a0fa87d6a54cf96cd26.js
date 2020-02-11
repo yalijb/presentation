3bb02d643c99891658b60f9f9cf522ad0665a68c
Yalianny <yaliannysg@gmail.com>
2020-02-11 11:23:22 -0400

    Updates

---
+++ precache-manifest.a00b46da71740a0fa87d6a54cf96cd26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f276a7e7c24964143618080cdb9ce92b",
+    "url": "/presentacion/index.html"
+  },
+  {
+    "revision": "6b8001d3b3a9bb4c334f",
+    "url": "/presentacion/static/css/main.f85d11e6.chunk.css"
+  },
+  {
+    "revision": "44f6ba8fb2b8ae14a0c3",
+    "url": "/presentacion/static/js/2.68625ce9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/presentacion/static/js/2.68625ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8001d3b3a9bb4c334f",
+    "url": "/presentacion/static/js/main.ccd4784f.chunk.js"
+  },
+  {
+    "revision": "22f2e5ac9549f1c8f5fa",
+    "url": "/presentacion/static/js/runtime-main.083fb4de.js"
+  }
+]);
